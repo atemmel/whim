2e5f8a2875a077f174758fde37ec1f3f5dca3761
atemmel <temmeladam@gmail.com>
2021-12-20 06:02:35 +0100

    Refactored live reload further

---
+++ include/live_reload.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include "html.hpp"
+#include "markdown.hpp"
+#include "result.hpp"
+
+#include <thread>
+
+struct HtmlTemplateLiveReloadState {
+	html::Template htmlTemplate;
+	std::mutex htmlTemplateMutex;
+	std::string path;
+};
+
+struct MarkdownLiveReloadState {
+	std::string markdownSource;
+	std::unique_ptr<md::Document> document;
+	std::mutex documentMutex;
+
+	std::string htmlOutput;
+	std::mutex htmlOutputMutex;
+	std::string path;
+
+	HtmlTemplateLiveReloadState* htmlTemplate;
+};
+
+
+ [[nodiscard]] auto setupLiveReloadState(std::string_view markdownPath, HtmlTemplateLiveReloadState& htmlState, MarkdownLiveReloadState& markdownState) -> Result<void>;
+
+auto reloadMarkdown(MarkdownLiveReloadState& markdownState) -> void;
+auto reloadHtmlOutput(MarkdownLiveReloadState& markdownState) -> void;
---
+++ src/live_reload.cpp
@@ -0,0 +1,68 @@
+#include "live_reload.hpp"
+
+#include "utils.hpp"
+
+#include <filesystem>
+
+ [[nodiscard]] auto setupLiveReloadState(std::string_view markdownPath, HtmlTemplateLiveReloadState& htmlState, MarkdownLiveReloadState& markdownState) -> Result<void> {
+	Result<void> result;
+	markdownState.path = markdownPath;
+	markdownState.htmlTemplate = &htmlState;
+	markdownState.markdownSource = consumeFile(markdownPath.data());
+	if(markdownState.markdownSource.empty()) {
+		std::string err = "Could not read file: ";
+		err += markdownPath;
+		return result.set(err);
+	}
+	markdownState.document = md::parse(markdownState.markdownSource);
+	if(!markdownState.document) {
+		return result.set("Could not build markdown document");
+	}
+
+	auto templateFile = markdownState.document->meta.find("template");
+	if(templateFile == markdownState.document->meta.end()) {
+		return result.set("No template path specified in markdown file header");
+	}
+
+	auto markdownDir = getPath(markdownPath);
+	htmlState.path = markdownDir;
+	htmlState.path += std::filesystem::path::preferred_separator;
+	htmlState.path += templateFile->second;
+
+	if(!endsWith(htmlState.path, ".html")) {
+		htmlState.path += ".html";
+	}
+
+	auto htmlSource = consumeFile(htmlState.path.c_str());
+	if(htmlSource.empty()) {
+		std::string err = "Could not read file at: ";
+		err += templateFile->second;
+		return result.set(err);
+	}
+
+	htmlState.htmlTemplate = html::compile(htmlSource);
+	markdownState.htmlOutput = htmlState.htmlTemplate.emit(*markdownState.document);
+
+	return result;
+}
+
+auto reloadMarkdown(MarkdownLiveReloadState& markdownState) -> void {
+	auto newMarkdownSource = consumeFile(markdownState.path.c_str());
+	auto newDocument = md::parse(newMarkdownSource);
+	markdownState.documentMutex.lock();
+	markdownState.document.swap(newDocument);
+	markdownState.documentMutex.unlock();
+}
+
+auto reloadHtmlOutput(MarkdownLiveReloadState& markdownState) -> void {
+	markdownState.documentMutex.lock();
+	markdownState.htmlTemplate->htmlTemplateMutex.lock();
+	auto newHtmlOutput = markdownState.htmlTemplate->htmlTemplate.emit(*markdownState.document);
+	markdownState.htmlTemplate->htmlTemplateMutex.unlock();
+	markdownState.documentMutex.unlock();
+
+	markdownState.htmlOutputMutex.lock();
+	markdownState.htmlOutput.swap(newHtmlOutput);
+	markdownState.htmlOutputMutex.unlock();
+}
+
