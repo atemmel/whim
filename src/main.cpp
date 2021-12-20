050c51ade1ba8f40dbdce952502f7069cc4f0a0c
atemmel <temmeladam@gmail.com>
2021-12-20 05:16:02 +0100

    Removed comments

--- src/main.cpp
+++ src/main.cpp
@@ -8,8 +8,6 @@
 #include <iostream>
 #include <thread>
 
-//void serverDemo();
-
 auto main(int argc, char** argv) -> int {
 	std::string markdownSource;
 
@@ -111,49 +109,3 @@ auto main(int argc, char** argv) -> int {
 		std::cerr << res.reason() << '\n';
 	}
 }
-
-/*
-void serverDemo() {
-	Http::Server server;
-
-	std::string_view markdown = R"(
----
-key = value
-title = cool title
-------
-epic content moment
-)";
-
-	std::string_view html = R"(
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<meta charset="UTF-8">
-		<title>$title</title>
-	</head>
-	<body>
-		<h3>$title</h3>
-		$main
-	</body>
-</html>
-)";
-
-	auto doc = md::parse(markdown);
-	auto htmlTemplate = html::compile(html);
-	auto output = htmlTemplate.emit(*doc);
-
-	auto root = [&](const Http::Message& message, TcpSocket client) {
-		std::string_view header = "HTTP/1.1 200 OK\n\n";
-		std::string_view response = html;
-		client.write(header);
-		client.write(output);
-	};
-
-	server.endpoint("/", root);
-
-	auto res = server.listen();
-	if(res.fail()) {
-		std::cerr << res.reason() << '\n';
-	}
-}
-*/
